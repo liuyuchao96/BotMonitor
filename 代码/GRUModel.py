dc119a2acae4758c456fdf2d1a5b41587cbfe56a
liuyuchao96 <liuychao96@163.com>
2021-01-12 20:42:05 +0800

    GRU数据预处理

--- 代码/GRUModel.py
+++ 代码/GRUModel.py
@@ -5,36 +5,51 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten, LSTM, GRU
 from keras.layers import Conv2D, MaxPooling2D
 import os
+import packet2idx as pi
 
 def load_data():
-    paths = ['mnist/train-labels-idx1-ubyte.gz', 'mnist/train-images-idx1-ubyte.gz',
-             'mnist/test-labels-idx1-ubyte.gz', 'mnist/test-images-idx1-ubyte.gz']
-    with open(paths[0], 'rb') as lbpath:
-        y_train = np.frombuffer(lbpath.read(), np.uint8, offset=8)
-    with open(paths[1], 'rb') as imgpath:
-        x_train = np.frombuffer(imgpath.read(), np.uint8, offset=16)
-    with open(paths[2], 'rb') as lapath:
-        y_test = np.frombuffer(lapath.read(), np.uint8, offset=8)
-    with open(paths[3], 'rb') as imgpath:
-        x_test = np.frombuffer(imgpath.read(), np.uint8, offset=16)
-    return (x_train, y_train), (x_test, y_test)
+    paths = ['source_data\\train', 'source_data\\test']
+    x_train = []
+    y_train = []
+    x_test = []
+    y_test = []
+    for dirname in os.listdir(paths[0]): # 带标签的文件夹名
+        path = os.path.join(paths[0], dirname)
+        for filename in os.listdir(path):
+            file_path = os.path.join(path, filename)
+            (x, y) = pi.getMatrixFromEachPacket(file_path)
+            x_train.append(x[0])
+            y_train.append(y[0])
+    for dirname in os.listdir(paths[1]): # 带标签的文件夹名
+        path = os.path.join(paths[1], dirname)
+        for filename in os.listdir(path):
+            file_path = os.path.join(path, filename)
+            (x, y) = pi.getMatrixFromEachPacket(file_path)
+            x_test.append(x[0])
+            y_test.append(y[0])
+    print(y_train)
+    return (x_train, y_train),  (x_test, y_test)
+
+# 加载数据
 (x_train, y_train), (x_test, y_test) = load_data()
 
-# 将类别转换成独热编码
 num_classes = 256
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
-
-x_train = keras.utils.to_categorical(x_train, num_classes)
-x_test = keras.utils.to_categorical(x_test, num_classes)
-
-x_train /= 255 # 归一化
-x_test /= 255 # 归一化
-
 epochs = 3
 batch_size = 8 # 8个数据包
 model = Sequential()
 input_dim = 256 # 每个字节经onehot编码后有256维
 input_length = 100 # 每个数据包包含100个字节
-model.add(LSTM(units=256, return_sequences=True, input_shape=(x_train.shape[0], 256)))
+model.add(LSTM(units=256, return_sequences=True, input_shape=(x_train[0].shape[0], num_classes)))
+model.add(GRU(units=256, return_sequences=True, input_shape=(x_train.shape[0], num_classes)))
 model.add(Dense(1))
+
+if __name__ == '__main__':
+
+    print(x_train[0].shape[0])
+    print(y_train)
+    # paths = ['source_data/train', 'source_data/test']
+    # for dirname in os.listdir(paths[0]): # 带标签的文件夹名
+    #     path = os.path.join(paths[0], dirname)
+    #     for filename in os.listdir(path):
+    #         file_path = os.path.join(path, filename)
+    #         print(file_path.split("\\")[1])
