af5c0d05ca8fffa6f43ca547cddec7626a9bccdf
liuyuchao96 <liuychao96@163.com>
2021-01-17 14:26:01 +0800

    move conflict

--- 代码/GRUModel.py
+++ 代码/GRUModel.py
@@ -2,7 +2,7 @@ import numpy as np
 import keras
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Flatten, LSTM, GRU
+from keras.layers import Dense, Activation, Flatten, LSTM, GRU
 from keras.layers import Conv2D, MaxPooling2D
 import os
 import packet2idx as pi
